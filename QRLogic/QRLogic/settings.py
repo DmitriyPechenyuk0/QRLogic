6349c50c7e508f5e8afad1c6f76e5eb043374f0c
Dmitriy <dmitriypechenyuk0@gmail.com>
2024-12-24 12:58:33 +0200

    add drop review by mail

--- QRLogic/QRLogic/settings.py
+++ QRLogic/QRLogic/settings.py
@@ -128,3 +128,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'qrlogic.practice@gmail.com'
+EMAIL_HOST_PASSWORD = 'sbgf tiak vzuz gtxv'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
