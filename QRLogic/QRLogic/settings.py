fea58b93d9cbd9aeac8757ffc2da9bbbc890a26c
Dmitriy <dmitriypechenyuk0@gmail.com>
2025-01-07 15:57:03 +0200

    create qrcode generating

--- QRLogic/QRLogic/settings.py
+++ QRLogic/QRLogic/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,3 +139,8 @@ EMAIL_HOST_USER = 'qrlogic.practice@gmail.com'
 EMAIL_HOST_PASSWORD = 'sbgf tiak vzuz gtxv'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- QRLogic/createqr_app/views.py
+++ QRLogic/createqr_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+import segno, os
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def render_ceateqr_app(request):
+    if request.method == 'POST':
+        url = request.POST.get('url')
+        qr = segno.make_qr(
+            content=url
+        )
+        qr.save(
+            out = os.path.abspath(__file__ + f'/../../media/qrcode.png'),
+            kind='png'
+        )
+
+        return render(request, 'createqr_app/createqrr.html')
     return render(request, 'createqr_app/createqrr.html')
