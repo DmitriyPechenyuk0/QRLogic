c830b07a76a4ea52c7f55525e39d40172bb65470
Nikita <nikitaemrih226@gmail.com>
2025-01-21 17:06:52 +0200

    finish

--- QRLogic/createqr_app/views.py
+++ QRLogic/createqr_app/views.py
@@ -6,15 +6,15 @@ from django.contrib.auth.models import User
 
 
 def render_ceateqr_app(request):
-    if request.method == 'POST':
-        url = request.POST.get('url')
-        qr = segno.make_qr(
-            content=url
-        )
-        qr.save(
-            out = os.path.abspath(__file__ + f'/../../media/qrcode.png'),
-            kind='png'
-        )
+    # if request.method == 'POST':
+    #     url = request.POST.get('url')
+    #     qr = segno.make_qr(
+    #         content=url
+    #     )
+    #     qr.save(
+    #         out = os.path.abspath(__file__ + f'/../../media/qrcode.png'),
+    #         kind='png'
+    #     )
 
         return render(request, 'createqr_app/createqrr.html')
-    return render(request, 'createqr_app/createqrr.html')
+    # return render(request, 'createqr_app/createqrr.html')
