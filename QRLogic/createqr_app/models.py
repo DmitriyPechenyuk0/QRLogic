dc140c92adce02abe03f25448d368706de1f16f6
Nikita <nikitaemrih226@gmail.com>
2025-01-31 16:58:08 +0200

    start of create qr

--- QRLogic/createqr_app/models.py
+++ QRLogic/createqr_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
+import segno
+import os
+from io import BytesIO
+from django.core.files.base import ContentFile
 # Create your models here.
+
+
+class QrCode(models.Model):
+    expire_date = models.IntegerField()
+    owner = models.ForeignKey(User, on_delete = models.CASCADE)
+    name = models.CharField(max_length=255)
+    url = models.URLField(max_length=200)
+    color = models.CharField(max_length=255)
+    background_color = models.CharField(max_length=255)
+    body_style = models.IntegerField()
+    frame_style = models.IntegerField()
+    square_style = models.IntegerField()
+    create_date = models.IntegerField()
+
+
+def save(request):
+        if request.method == 'POST':
+            url = request.POST.get('url')
+            qr = segno.make_qr(
+                content=url)
+            qr.save(out = os.path.abspath(__file__ + f'/../../media/qrcode.png', kind='png'))
+
+    
+
+
+
+
+
--- QRLogic/createqr_app/views.py
+++ QRLogic/createqr_app/views.py
@@ -1,20 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import segno, os
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .models import QrCode
+from .form import QRCodeForm
 
 # Create your views here.
 
 
-def render_ceateqr_app(request):
+#def render_ceateqr_app(request):
     # if request.method == 'POST':
     #     url = request.POST.get('url')
     #     qr = segno.make_qr(
     #         content=url
     #     )
     #     qr.save(
-    #         out = os.path.abspath(__file__ + f'/../../media/qrcode.png'),
-    #         kind='png'
-    #     )
+    #         out = os.path.abspath(__file__ + f'/../../media/qrcode.png', kind='png'),
+    #         )
+        
+    #     return render(request, 'createqr_app/createqrr.html')
+@login_required
+def create_qr(request):
+    if request.method == 'POST':
+        form = QRCodeForm(request.POST)
+        if form.is_valid():
+            qr_instance = form.save(commit=False)
+            qr_instance.owner = request.owner
+            qr_instance.save()
+            return redirect('qr_list')
+    else:
+        form = QRCodeForm()
+    return render(request, 'createqrr.html', {'form': form})
 
-        return render(request, 'createqr_app/createqrr.html')
-    # return render(request, 'createqr_app/createqrr.html')
+    
+@login_required
+def qr_list(request):
+    qrcodes = QrCode.objects.filter(user=request.user) 
+    return render(request, 'yourqrr.html', {'qrcodes': qrcodes})
