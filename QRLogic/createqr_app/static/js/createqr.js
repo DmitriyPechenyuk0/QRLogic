fbca7d5fe14aac9e2718a771eb80d3cc49c47d1d
Dmitriy <dmitriypechenyuk0@gmail.com>
2025-02-05 01:07:13 +0200

    remake of color inputs on createqr page

--- QRLogic/createqr_app/static/js/createqr.js
+++ QRLogic/createqr_app/static/js/createqr.js
@@ -2,10 +2,25 @@ const InputSubstrate = document.querySelector('.input-substrate');
 const inputPrimary = document.querySelector('.input-primary');
 const pSubstrate = document.querySelector('.p-substrate');
 const pPrimary = document.querySelector('.p-primary');
+let labelPrimary = document.querySelector('.label-primary')
+let labelSubstrate = document.querySelector('.label-substrate')
+
+
+const defaultColor = InputSubstrate.value;
+labelSubstrate.style.backgroundColor = defaultColor;
+pSubstrate.textContent = defaultColor;
+
+const defaultColor2 = inputPrimary.value;
+labelPrimary.style.backgroundColor = defaultColor2;
+pPrimary.textContent = defaultColor2;
+
+
 
 InputSubstrate.addEventListener('input', () => {
     pSubstrate.textContent = InputSubstrate.value;
+    labelSubstrate.style.backgroundColor = InputSubstrate.value;
 });
 inputPrimary.addEventListener('input', () => {
     pPrimary.textContent = inputPrimary.value;
+    labelPrimary.style.backgroundColor = inputPrimary.value;
 });
