29d4d53a1bf6c2db74efe6ce85013e45ffa1baf3
Nikita <nikitaemrih226@gmail.com>
2025-02-07 20:59:39 +0200

    commit

--- QRLogic/yourqr_app/views.py
+++ QRLogic/yourqr_app/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import os
+from django.contrib.auth.decorators import login_required
+from user_app.models import Profile
+from createqr_app.models import QrCode
+from QRLogic import settings
+
+
+
+
 
 # Create your views here.
 
+@login_required
 def render_yourqr_app(request):
-    context = {'page': 'myqr'}
+    profile = Profile.objects.get(user=request.user)
+    qrcodes = QrCode.objects.filter(owner=profile).order_by('-create_date')
+
+    for qr in qrcodes:
+        filepath = os.path.join(settings.MEDIA_ROOT, f"{request.user.username}_{request.user.id}/{qr.name}")
+        if os.path.exists(filepath):
+            qr.image_url = f"{settings.MEDIA_URL}{request.user.username}_{request.user.id}/{qr.name}"
+    
+        
+
+    context = {
+        'page': 'myqr',
+        'qrcodes': qrcodes
+    }
+
     return render(request, 'yourqr_app/yourqrr.html', context)
