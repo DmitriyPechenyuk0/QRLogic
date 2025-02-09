ac039f7053e227959bf29c2709a8d9cca8aaa222
Dmitriy <dmitriypechenyuk0@gmail.com>
2025-02-09 05:56:13 +0200

    create myqr page without filters

--- QRLogic/yourqr_app/views.py
+++ QRLogic/yourqr_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-
+from createqr_app.models import QrCode
 # Create your views here.
 
 def render_yourqr_app(request):
     context = {'page': 'myqr'}
+    
+    qrcodes = QrCode.objects.filter(owner=request.user.profile)
+    context = {'page': 'myqr', 'qrcodes': qrcodes}
+
     if request.user.is_authenticated:
         return render(request, 'yourqr_app/yourqrr.html', context=context)
+    
     else:
         return render(request, 'createqr_app/authentication_required.html', context=context)
