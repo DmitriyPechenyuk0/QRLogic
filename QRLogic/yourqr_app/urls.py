3cc89a872bc8b379485e7cbd06fcaeac1e91ad39
Nikita <nikitaemrih226@gmail.com>
2025-02-21 14:39:03 +0200

    create button download and delete

--- QRLogic/yourqr_app/urls.py
+++ QRLogic/yourqr_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from yourqr_app.views import render_yourqr_app
+from yourqr_app.views import render_yourqr_app, download_qrcode
 
 urlpatterns= [
     path('yourqr/', render_yourqr_app, name= 'yourqr_app'),
--- QRLogic/yourqr_app/views.py
+++ QRLogic/yourqr_app/views.py
@@ -5,6 +5,9 @@ from user_app.models import Profile
 from createqr_app.models import QrCode
 from QRLogic import settings
 from datetime import datetime
+from django.shortcuts import get_object_or_404
+
+
 
 
 
@@ -17,6 +20,18 @@ def render_yourqr_app(request):
     profile = Profile.objects.get(user=request.user)
     qrcodes = QrCode.objects.filter(owner=profile).order_by('-create_date')
     context = {'page': 'myqr'}
+
+    if request.method == "POST" and "delete_qr" in request.POST:
+        qr_id = request.POST.get("delete_qr")
+        qr = get_object_or_404(QrCode, id=qr_id, owner__user=request.user)
+        
+        file_path = os.path.join(settings.MEDIA_ROOT, f"{request.user.username}_{request.user.id}", qr.name)
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        
+        qr.delete()
+
+
     if request.method == "POST":
         date_filter = request.POST.get('date_filter')
         if date_filter:  
@@ -42,8 +57,11 @@ def render_yourqr_app(request):
     context = {
         'page': 'myqr',
         'qrcodes': qrcodes,
-        'qr_images': qr_images  
+        'qr_images': qr_images,
     }
 
     return render(request, 'yourqr_app/yourqrr.html', context)
+
+
+
 
