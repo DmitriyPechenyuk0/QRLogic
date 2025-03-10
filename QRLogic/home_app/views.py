1bf562ee27dcb42858dccd5c9b11ae5cbccd1c3a
Dmitriy <dmitriypechenyuk0@gmail.com>
2025-03-10 18:00:10 +0200

    add comments to views.py home

--- QRLogic/home_app/views.py
+++ QRLogic/home_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 # Create your views here.
-
+# Creating render function to home page. | Створення функції рендерингу для сторінки home.
 def render_home_app(request: HttpRequest):
+    # Passes the page name to the context. | Передається в context назва сторінки.
     context = {'page': 'home'}
-
+    # Display the home page to the user. | Відображаємо сторінку home користувачу.
     return render(request, 'home_app/home.html', context=context)
